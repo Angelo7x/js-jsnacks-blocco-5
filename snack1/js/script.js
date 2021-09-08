f6736c02008e589396544a8ae33e1ded6ccf1a87
Angelo7x <abennici96@gmail.com>
2021-09-08 11:29:27 +0200

    array

---
+++ snack1/js/script.js
@@ -0,0 +1,64 @@
+var zucchine = 
+[
+    {
+        nome : "Nera di Milano",
+        peso : 272,
+        lunghezza : 20
+    },
+
+    {
+        nome : "Alberello",
+        peso : 540,
+        lunghezza : 30
+    },
+
+    {
+        nome : "Tonda di Nizza",
+        peso : 320,
+        lunghezza : 50
+    },
+
+    {
+        nome : "Lunghe fiorentine",
+        peso : 328,
+        lunghezza : 22
+    },
+
+    {
+        nome : "Trombetta d'Albenga",
+        peso : 120,
+        lunghezza : 34
+    },
+
+    {
+        nome : "Zucchino giallo Golden",
+        peso : 240,
+        lunghezza : 44
+    },
+
+    {
+        nome : "Zucchino romanesco",
+        peso : 300,
+        lunghezza : 54
+    },
+
+    {
+        nome : "Zucchino ortolano di Faenza",
+        peso : 280,
+        lunghezza : 30
+    },
+
+    {
+        nome : "Zucchino siciliano",
+        peso : 400,
+        lunghezza : 27
+    },
+
+    {
+        nome : "Zucchina bianca triestina",
+        peso : 380,
+        lunghezza : 92
+    }
+
+];
+
